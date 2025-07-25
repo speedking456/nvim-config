892da73ec65d822c0a6a87aed7bac44d5101d2b3
speedking456 <20252229+speedking456@users.noreply.github.com>
2025-07-25 09:20:50 -0700

    add org mode (disabled for now) and change which-key binds

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+return {
+  "folke/which-key.nvim",
+  opts = {
+    scroll_options = {
+      scroll_down = "<c-Down>", -- Original: "<c-d>"
+      scroll_up = "<c-Up>", -- Original: "<c-u>"
+    },
+  },
+}
