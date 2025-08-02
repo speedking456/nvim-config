5cc5a1420d825544201ad5b03d5e816d80ac760d
speedking456 <20252229+speedking456@users.noreply.github.com>
2025-08-01 18:29:41 -0700

    bump

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/which-key.nvim",
+  opts = {
+    scroll_options = {
+      scroll_down = "<c-Down>", -- Original: "<c-d>"
+      scroll_up = "<c-Up>", -- Original: "<c-u>"
+    },
+  },
+}
